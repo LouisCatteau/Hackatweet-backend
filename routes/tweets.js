3543c89b5e74b0486f80b58fd573aa56d5d6411d
Louis Catteau <louiscatteauo@gmail.com>
2024-05-03 11:40:21 +0200

    modif de la route post nouveau tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -7,13 +7,14 @@ const User = require('../models/users');
 
 
 router.post('/newTweet', (req, res) => {
-    findOne({token:req.body.tweet.token})
+    User.find({token : req.body.token})
     .then(data=>{
+        console.log(data[0]._id)
         const newTweet = new Tweet({
-            message: req.body.tweet.message,
-            date: req.body.tweet.date,
+            message: req.body.message,
+            date: req.body.date,
             nbLike: 0,
-            user: data._id
+            user: data[0].id
         });
         newTweet.save().then(newTweet => {
             res.json({ result: true, newTweet });
@@ -30,7 +31,6 @@ router.get('/allTweets', (req, res) => {
     Tweet.find()
         .populate('user')
         .then(tweets => {
-
             res.json({ result: true, tweets });
         })
 })
